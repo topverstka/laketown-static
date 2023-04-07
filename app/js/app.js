5bfdd4441ae116627d058992c60387f3c591655f
denissgraf <37991639+denissgraf@users.noreply.github.com>
2023-04-07 09:06:47 +0300

    main page

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,31 @@ $(document).ready(function() {
       .animate({marginTop:40},6000, loop); // callback
   }
 
-  loop();
+  //loop();
+
+
+  $('#menuOpen').on('click', function(){
+    $('.top-menu').addClass('active');
+  });
+  $('#closeMenu').on('click', function(){
+    $('.top-menu').removeClass('active');
+  });
+
+
+  (function(elements) {
+    var i = -1;
+    var className = 'active';
+    if (!elements.length) {
+      return false;
+    }
+    function step() {
+      elements.eq(i).removeClass(className);
+      if (++i >= elements.length) {
+        i = 0;
+      };
+      elements.eq(i).addClass(className);
+      setTimeout(step, 1200);
+    }
+    step();
+  }($('header #slogans').children()));
 });
